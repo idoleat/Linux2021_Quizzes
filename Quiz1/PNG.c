3d058b57cd622e7b3edf8bc7e35fb3f17439dd50
C.W. Chien <dppss92132@gmail.com>
2021-03-16 12:56:35 +0800

    Change the pow function from math.h to my own
    The pow() from math.h returns double, to avoid rounding errors I write
    my own power function. I still need to research of what's risk to cast
    double into long long int. At least using my own one there are no risks
    
    Also I've heard some comments saying that don't use pow().

--- Quiz1/PNG.c
+++ Quiz1/PNG.c
@@ -1,22 +1,34 @@
 #include<stdlib.h>
-#include<math.h>
 
+/* A struct hold all the information that ACORN need.
+ */
 struct ACORN {
     int precision_multiplier;
-    int K;
-    int N;
-    int M;
-    int seed;
+    int K; /* order */
+    int N; /*  */
+    long long int M;
+    long long int seed;
     int auto_correct_seed; // it's a bool
-    int *initial_value;
+    long long int *initial_value;
 };
 
-void initializeACORN(struct ACORN *acorn)
+long long int power(long long base, int exp)
 {
-    acorn->precision_multiplier = 1;
+    long long result = 1;
+    while (exp > 0){
+        result *= base;
+        exp -= 1;
+    }
+
+    return result;
+}
+
+void InitializeACORN(struct ACORN *acorn)
+{
+    acorn->precision_multiplier = 2; // due to long long it only can be up to 2
     acorn->K = 10000;
     acorn->N = 1;
-    acorn->M = pow(2, 30 * acorn->precision_multiplier); // Can I acutally use pow()? Or define a pow by myself?
+    acorn->M = power(2, 30 * acorn->precision_multiplier);
     acorn->seed = 0;
     acorn->auto_correct_seed = 1;
     acorn->initial_value = NULL;
@@ -27,7 +39,8 @@ int IsSeedValid(struct ACORN *acorn)
     if (acorn->seed <= 0)
         return 0;
     if (acorn->seed % 2 == 0){
-        if (acorn->auto_correct_seed) acorn->seed -= 1;
+        /* This may cause collision with other seed. Could have a better way. */
+        if (acorn->auto_correct_seed) acorn->seed -= 1; 
         return 0;
     }
 
@@ -40,7 +53,8 @@ void SetProperty(struct ACORN *acorn, int pricision, int order, int auto_correct
     acorn->auto_correct_seed = auto_correct;
 }
 
-void Random()
+long long int *Random(int seed)
 {
-
+    struct ACORN acorn;
+    InitializeACORN(&acorn);
 }
