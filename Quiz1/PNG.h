0cef3e61b1e23f1bad51be2c0b65d253177e5874
C.W. Chien <dppss92132@gmail.com>
2021-03-17 00:01:59 +0800

    Add header file of PNG.c
    PNG.h is added

---
+++ Quiz1/PNG.h
@@ -0,0 +1,11 @@
+#ifndef __PNG_H
+#define __PNG_H
+
+struct ACORN *NewACORN();
+void SetProperty(struct ACORN *acorn, int pricision, int order, int auto_correct);
+void SetInitialValue(struct ACORN *acorn, long long int *initVal);
+void FreeACORN(struct ACORN *acorn);
+float *RandomArray(struct ACORN *acorn, long long seed, int length);
+float Random(struct ACORN *acorn, long long seed);
+
+#endif
